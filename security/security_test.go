2b35438c4b661494d9a7a82d8f4795246b0d5e69
axiangcoding <axiangcoding@gmail.com>
2022-05-11 23:18:52 +0800

    feat: add security_test.go

---
+++ security/security_test.go
@@ -0,0 +1,41 @@
+package security
+
+import "testing"
+
+var examples = []struct {
+	plainPwd string
+	hashPwd  string
+}{
+	{"example1", "$2a$10$7n6u96U2UD5polWnnhW7.eBXDrGt.VgKpQDARtiqhgjelsy0sCN/S"},
+	{"p@ssword", "$2a$10$C3ybIDyzsAhx1MgA33yVguC5oR0zyHP/M2ExaKTv9wioNqWz9WfDq"},
+	{"abcdefghi", "$2a$10$29ZeqsSWFo1kQ1BfU3Cot.d7tOftergFVyZCK2e/.QBfnx3WaCZ3y"},
+	{"123456789", "$2a$10$aQOUsb6QbLdP6qB8uVt6Xug8jiA.ZIRtE1hxhXgH/7mBfGKtROhcq"},
+	{"111111111111", "$2a$10$dEWSP91xmDdRODB/Mvkh3OJV3xzo4ZrxVHgVjeq3ZPc/geDhJjZai"},
+	{"ILoveAxth", "$2a$10$kzl8SAEkfYCl8g8.wRxYn.TQ4iRe6qnVUW5qzxI6oZcS7/H.POHZG"},
+}
+
+func TestGeneratePwd(t *testing.T) {
+	for _, example := range examples {
+		caseName := "case '" + example.plainPwd + "'"
+		t.Run(caseName, func(t *testing.T) {
+			_, err := GeneratePwd(example.plainPwd)
+			if err != nil {
+				t.Error(err)
+			}
+		},
+		)
+	}
+}
+
+func TestComparePwd(t *testing.T) {
+	for _, example := range examples {
+		caseName := "case '" + example.plainPwd + "'"
+		t.Run(caseName, func(t *testing.T) {
+			err := ComparePwd(example.hashPwd, example.plainPwd)
+			if err != nil {
+				t.Error(err)
+			}
+		},
+		)
+	}
+}
